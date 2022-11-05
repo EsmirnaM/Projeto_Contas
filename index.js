0a4ece8c0cf084a35d487add96a4a5c21eee7b16
Esmirna <esmirnaamneris@gmail.com>
2022-11-05 10:51:10 -0300

    action criar conta

--- index.js
+++ index.js
@@ -26,7 +26,21 @@ function operation(){
         ],
     },
 ])
-.then()
+.then((answer) => {
+
+    const action = answer['action']
+
+    if (action === 'Criar Conta') {
+        createAccount()
+    } 
+
+})
 .catch((err) => console.log(err))
 }
 
+//criar conta
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parábens por escolher o nosso banco'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
